56151eb92291522a6548b1056f2e6f2348467043
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2025-01-09 03:36:33 +0530

    new changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const USER = mongoose.model("INSTAUSER");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res) => {
